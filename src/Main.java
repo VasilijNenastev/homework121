1625331ca81c9b1a9546f108dd1ed37f1d77caab
1 <vasilijnenastev@gmail.com>
2024-10-16 22:49:09 +0400

    HW2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        var dog=8.0;
+        System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        var paper=763789;
+        System.out.println(paper);
+        var dog1=8.0+4.0;
+        System.out.println(dog1);
+        var cat1=3.6+4.0;
+        System.out.println(cat1);
+        var paper1=763789+4.0;
+        System.out.println(paper1);
+        var dog2=dog1-3.5;
+        System.out.println(dog2);
+        var cat2=cat1-1.6;
+        System.out.println(cat2);
+        var paper2=paper1-7639;
+        System.out.println(paper2);
+        var friend=19;
+        System.out.println(friend);
+        var friend1=friend+2;
+        System.out.println(friend1);
+        var friend2=friend1/7;
+        System.out.println(friend2);
+        var frog=3.5;
+        System.out.println(frog);
+        var frog1=frog*10;
+        System.out.println(frog1);
+        var frog2=frog1/3.5;
+        System.out.println(frog2);
+        var frog3=frog2+4;
+        System.out.println(frog3);
+        var mass=78.2+82.7;
+        System.out.println(mass);
+        var mass1=82.7-78.2;
+        System.out.println(mass1);
+        var mass2=82.7%78.2;
+        System.out.println(mass2);
+        var main=640/8;
+        System.out.println("Всего работников компании-" +main+ "человек");
+        var main2=main+94;
+        var taim=main2*8;
+        System.out.println("Если в компании работает" +main2+ "человек, то всего" +taim+ "часов работы может быть поделено между сотрудниками");
+
+
     }
 }
